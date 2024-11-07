81217d89f04c71a321adf3af4236706b2610ac0f
Krishna Prasath <kp@Krishnas-MacBook-Air.local>
2024-11-07 23:02:59 +0530

    add geter&setter

--- Parking_Lot_Management/src/main/ParkingSpace.java
+++ Parking_Lot_Management/src/main/ParkingSpace.java
@@ -3,40 +3,34 @@ package Parking_Lot_Management.src.main;
 public class ParkingSpace {
     private boolean isAvailable;
     private Vehicle currentVehicle;
-
-    // Static variable to track the number of parking spaces in use
     private static int parkingSpacesInUse = 0;
 
-    // Constructor
     public ParkingSpace() {
         this.isAvailable = true;
     }
 
-    // Member function 1: Check availability
     public boolean isAvailable() {
         return this.isAvailable;
     }
 
-    // Member function 2: Assign a vehicle to the parking space
     public void assignVehicle(Vehicle vehicle) {
-        this.currentVehicle = vehicle;
-        this.isAvailable = false;
-        parkingSpacesInUse++; // Increment static counter
+        if (isAvailable) {
+            this.currentVehicle = vehicle;
+            this.isAvailable = false;
+            parkingSpacesInUse++;
+        }
     }
 
-    // Member function 3: Free the parking space
     public void freeSpace() {
-        this.currentVehicle = null;  // Setting it to null to indicate no vehicle is assigned
+        this.currentVehicle = null;
         this.isAvailable = true;
-        parkingSpacesInUse--; // Decrement static counter
+        parkingSpacesInUse--;
     }
 
-    // Member function 4: Get current vehicle
     public Vehicle getCurrentVehicle() {
         return this.currentVehicle;
     }
 
-    // Static function to get the number of parking spaces in use
     public static int getParkingSpacesInUse() {
         return parkingSpacesInUse;
     }
